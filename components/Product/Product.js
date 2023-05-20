ff9eab96ab0dd0d086ee2dea9e76ee62eb81f5eb
Sumit Kumar Kushwaha <SKKushwaha@bpl.netlink.com>
2023-05-20 22:31:25 +0530

    refactor

---
+++ components/Product/Product.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../Layout";
+
+function Product() {
+  return (
+    <Layout>
+      <div className="w-full h-screen bg-slate-300">
+        <nav></nav>
+      </div>
+    </Layout>
+  );
+}
+
+export default Product;
---
+++ pages/products.js
@@ -0,0 +1,12 @@
+import Product from "@/components/Product/Product";
+import React from "react";
+
+function productPage() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+
+export default productPage;
