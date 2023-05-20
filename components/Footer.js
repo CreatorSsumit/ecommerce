6698ec4fb904aafd459f17890e781883d26db190
Sumit Kumar Kushwaha <SKKushwaha@bpl.netlink.com>
2023-05-21 01:26:40 +0530

    product done

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className={`text-white ${playfairDisplay.className} bg-[#1f2d2c]`}>
       <footer class=" body-font">
-        <div class="container px-5 py-24 mx-auto">
+        <div class="container px-5 py-16 mx-auto">
           <div class="flex flex-wrap md:text-left text-center order-first">
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium  tracking-widest text-sm mb-3">
